e9c3b404a528a09187bef8e19bd706dc123e37bc
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-19 15:22:49 +0000

    Adds database cleaner and ensures test run

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,18 +5,27 @@ require File.join(File.dirname(__FILE__), '..', 'app/app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require './app/app'
+require 'web_helpers'
+require 'database_cleaner'
 
 Capybara.app = Chitter
 
 RSpec.configure do |config|
   config.include Capybara::DSL
 
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+
   end
 
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
+  config.before(:each) do
+    DatabaseCleaner.start
   end
 
+  config.after(:each) do
+    DatabaseCleaner.clean
+
+  end
 end
