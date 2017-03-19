f88293e43ff38a39c64e5a6312dcd877b9aaa706
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-19 11:29:49 +0000

    moves app.rb in to app folder

---
+++ app/app.rb
@@ -0,0 +1,14 @@
+require 'sinatra/base'
+
+class Chitter < Sinatra::Base
+  get '/' do
+    'Hello Chitter!'
+  end
+
+  get '/users/new' do
+    
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
