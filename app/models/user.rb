8f6a7e8128e6f7234091ca9b538e8b2c451c05b5
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-19 15:20:27 +0000

    Adds user to database

---
+++ app/models/user.rb
@@ -0,0 +1,38 @@
+require 'data_mapper'
+require 'dm-postgres-adapter'
+require 'bcrypt'
+
+class User
+
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String, required: true
+  property :username, String, required: true, unique: true
+  property :email, String, required: true, unique: true
+  property :password_digest, Text, required: true
+
+  attr_reader :password
+  attr_accessor :password_confirmation
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(username, password)
+    user = first(username: username)
+
+    if user && BCrypt::Password.new(user.password_digest) == password_digest
+      user
+    else
+      nil
+    end
+
+  end
+
+end
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/chitter_#{ENV['RACK_ENV']}")
+DataMapper.finalize
+DataMapper.auto_upgrade!
