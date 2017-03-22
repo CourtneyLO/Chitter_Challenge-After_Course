74d2d8b2be41c33de85b4f96ebd7510f11e8ead5
Courtney Osborn <courtney.lee.osborn@gmail.com>
2017-03-22 14:57:01 +0000

    Deletes unnecessary code

--- app/app.rb
+++ app/app.rb
@@ -1,10 +1,8 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-# require_relative './models/user.rb'
-# require_relative './models/peep.rb'
 require_relative 'data_mapper_setup'
-# require 'data_mapper'
+
 
 
 class Chitter < Sinatra::Base
@@ -94,7 +92,7 @@ helpers do
 
 end
 
-enable :run
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
